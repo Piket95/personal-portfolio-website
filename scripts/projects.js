2bf82e5491041e1f83190f6789e383630b52c090
Dennis Adam <28174344+Piket95@users.noreply.github.com>
2025-09-27 17:26:33 +0200

    filled projects section with content and adjusted css

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,59 +1,54 @@
 const projects = [
+    // {
+    //     'title': 'Vorlage',
+    //     'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
+    //     'github': 'https://github.com/Piket95',
+    //     'visit': 'https://github.com/Piket95'
+    // },
     {
-        'title': 'Projekt 1',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
+        'title': 'Shutdown Timer',
+        'description': 'Mein erstes eigenes kleines Tool in Visual Basic. Ein Tool um automatisch den PC zu einer bestimmten Uhrzeit oder einem Timer herunterzufahren.',
+        'github': 'https://github.com/Piket95/ShutdownTimer_2_0',
     },
     {
-        'title': 'Projekt 2',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
-        'visit': 'https://github.com/Piket95'
+        'title': 'Custom DynDNS Service',
+        'description': 'Ein eigener kleiner Dienst um dynamische IP Adressen mit einer fixen Domaine zu verknüpfen. Änlich wie No-IP, Dynu oder Cloudflare DDNS.',
+        'github': 'https://github.com/Piket95/custom-python-ddns',
     },
     {
-        'title': 'Projekt 3',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
-        'visit': 'https://github.com/Piket95'
+        'title': 'Pokémon Origin Living Dex - Discord Rich Presence',
+        'description': 'Eine kleine Discord Rich Presence Applikation, die den aktuellen Fortschritt im eigenen Pokémon Origin Living Dex Projekt in Discord präsentiert.',
+        'github': 'https://github.com/Piket95/pokemon-origin-livingdex-discord-rich-presence',
     },
     {
-        'title': 'Projekt 4',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
+        'title': 'WG Finanzen',
+        'description': 'Eine kleine Android App im Rahmen eines Studenten Projekts mit Web API Anbindung zur Verwaltung einer WG und deren Finanzen.',
+        'github': 'https://github.com/Piket95/WGFinanzen',
     },
     {
-        'title': 'Projekt 5',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
+        'title': 'Car Analytics',
+        'description': 'Eine Webapplikation mit Angular im Rahmen unseres Studienprojektes. Verwaltung von Fahrzeugen und deren Statistiken.',
+        'github': 'https://github.com/Piket95/CarAnalytics',
     },
     {
-        'title': 'Projekt 6',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
-        'visit': 'https://github.com/Piket95'
+        'title': 'Games Event Data API - WIP',
+        'description': 'Eine API die automatisch im Internet nach Gamecodes und Ingame Event Daten suchen soll und diese in einer API bereitstellen soll.',
+        'github': 'https://github.com/Piket95/games-event-data-api',
     },
     {
-        'title': 'Projekt 7',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
+        'title': 'Chatprogramm',
+        'description': 'Ein kleines CLI Chatprogramm in Java, das im Rahmen der Studienveranstaltung \"Entwicklung verteilter Anwendungen in Java\" entwickelt wurde.',
+        'github': 'https://github.com/Piket95/EVA_Chatprogramm',
     },
     {
-        'title': 'Projekt 8',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
-        'visit': 'https://github.com/Piket95'
+        'title': 'Mein eigener Server',
+        'description': 'Mein eigener Server gehostet in der Cloud. Hier liste ich in Zukunft auf, welche Applikationen ich darauf laufen lasse!',
+        'visit': 'https://github.com/Piket95', // link zu einer Doku oder so, die die Applikationen auflistet
     },
     {
-        'title': 'Projekt 9',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
-        'visit': 'https://github.com/Piket95'
-    },
-    {
-        'title': 'Projekt 10',
-        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus, nisl ut dignissim aliquet.',
-        'github': 'https://github.com/Piket95',
-        'visit': 'https://github.com/Piket95'
+        'title': 'Meine Skripte',
+        'description': 'Hier will ich in Zukunft auch meine Skripte die ich über meine Zeit auf Linux geschrieben habe auflisten und teilen.',
+        // 'visit': 'https://github.com/Piket95',
     },
 ]
 
@@ -74,7 +69,9 @@ function generateProjectCards(container) {
             card.className = 'project-card';
             card.innerHTML = template;
             card.querySelector('h3').textContent = project.title;
+            card.querySelector('h3').title = project.title;
             card.querySelector('p').textContent = project.description;
+            card.querySelector('p').title = project.description;
 
             if (project.image && project.image !== '') {
                 card.querySelector('img').src = project.image;
